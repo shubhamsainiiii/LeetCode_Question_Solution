57f9845f3b05c1c943907c12fa12759a6085025c
Shubham Saini <136838066+shubhamsainiiii@users.noreply.github.com>
2024-11-05 22:50:17 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (47.68%) - LeetSync

---
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (strs[j][i] < strs[j - 1][i]) {
+                    count++;
+                    break;  
+                }
+            }
+        }
+        return count;
+    }
+};
